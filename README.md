d2610ce79610c7ee3dca8f6d2757a03b9c6c8bc8
汪洋 <wang.yang@hbtown.com>
2022-08-01 14:29:02 +0800

    update READ.md

--- README.md
+++ README.md
@@ -1,18 +1,25 @@
 # better_lanhuapp_for_flutter
 从蓝湖下载的图片快速导入到Flutter项目中, 并生成对应的代码
 
-#### 主要代码
+## 处理流程
+1. 遍历找到正确的从蓝湖下载的iOS系统对应的图片zip文件
+2. 过滤条件: 后缀是zip, zip文件名不包含中文, zip包含ios对应的三种规格图片文件
+3. 解压zip到项目中指定的位置, 并使用zip文件名对图片进行重命名
+4. 在项目中生成图片路径代码
+
+## 用法
+1. 在项目根目录生成 env.py
 ```python
-# 遍历找到正确的从蓝湖下载的iOS系统对应的图片zip文件
-for f in files:
-    path = os.path.join(root, f)
-    # 过滤条件: 后缀是zip, zip文件名不包含中文, zip包含ios对应的三种规格图片文件, 项目中没有导入过这个图片
-    if path.endswith('.zip') and not contain_chinese(f) and is_lanhu_zip(path) and not exists_in_project(path):
-        # 解压zip到项目中指定的位置, 并使用zip文件名对图片进行重命名
-        unzip(path)
-        # 在项目中生成图片路径代码
-        generate_code(path)
+# 从蓝湖下载的图片目录
+download_dir = '/Users/xxx/Downloads'
 
-        print("成功导入 " + f)
+# flutter项目图片目录
+project_image_dir = '/Users/xxx/static/images'
 
+# flutter项目图片路径代码文件
+project_code_file = '/Users/xxx/lib/config/image_path.dart'
+
+# 如果项目中已经导入了同名文件是否强制覆盖
+isForceCover = True
 ```
+2. 运行main.py
--- main.py
+++ main.py
@@ -3,10 +3,11 @@
 import os
 import re
 import shutil
+import time
 import zipfile
 
 # 检测文件名是否包含中文字符,
-from env import download_dir, project_image_dir, project_code_file
+from env import download_dir, project_image_dir, project_code_file, isForceCover
 
 
 def contain_chinese(check_str):
@@ -92,17 +93,24 @@ def generate_code(file_path):
 
 
 if __name__ == '__main__':
-    print()
     for root, dirs, files in os.walk(download_dir):
 
         # 遍历找到正确的从蓝湖下载的iOS系统对应的图片zip文件
         for f in files:
             path = os.path.join(root, f)
-            # 过滤条件: 后缀是zip, zip文件名不包含中文, zip包含ios对应的三种规格图片文件, 项目中没有导入过这个图片
-            if path.endswith('.zip') and not contain_chinese(f) and is_lanhu_zip(path) and not exists_in_project(path):
-                # 解压zip到项目中指定的位置, 并使用zip文件名对图片进行重命名
-                unzip(path)
-                # 在项目中生成图片路径代码
-                generate_code(path)
+            # 过滤条件: 后缀是zip, zip文件名不包含中文, zip包含ios对应的三种规格图片文件
+            if path.endswith('.zip') and not contain_chinese(f) and is_lanhu_zip(path):
+                # 项目中没有导入过这个图片
+                if not exists_in_project(path):
+                    # 解压zip到项目中指定的位置, 并使用zip文件名对图片进行重命名
+                    unzip(path)
+                    # 在项目中生成图片路径代码
+                    generate_code(path)
 
-                print("成功导入 " + f)
+                    print("成功导入 " + f)
+
+                elif isForceCover:
+                    # 解压zip到项目中指定的位置, 并使用zip文件名对图片进行重命名
+                    unzip(path)
+
+                    print("成功覆盖 " + f)
